6c781a72b85d4829a575d0caffd4334ea7450766
Idongesit Idiong <idiong@tealcommunications.com>
2023-10-27 11:53:51 -0400

    fix '#' in url

--- public/js/myScript.js
+++ public/js/myScript.js
@@ -10,15 +10,31 @@ function myFunction() {
         x.className = "topnav";
         x.style.display = "block";
     }
-   
+
 }
 
 function closeSidebar() {
     let y = document.getElementById("myTopnav");
-    if (y.style.display === "block"){
+    if (y.style.display === "block") {
         y.style.display = "none";
         y.className += " responsive";
     }
 }
 
+
+function goHome() {
+    document.getElementById("home").scrollIntoView()
+}
+function goAbout() {
+    document.getElementById("about").scrollIntoView()
+}
+function goProducts() {
+    document.getElementById("products").scrollIntoView()
+}
+function goContact() {
+    document.getElementById("contact").scrollIntoView()
+}
+function goContact() {
+    document.getElementById("contact").scrollIntoView()
+}
 
