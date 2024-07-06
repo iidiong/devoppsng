b9094aab26b5ec12c2be814bb8b193fa34722651
Idongesit Idiong <idiong@tealcommunications.com>
2024-07-06 02:33:46 -0400

    fix issue with moble display

--- index.js
+++ index.js
@@ -9,4 +9,5 @@ let port = process.env.PORT;
 if (port == null || port == "") {
     port = 3000;
 }
+
 app.listen(port, () => console.log("App is listening on port 3000"))
--- public/js/myScript.js
+++ public/js/myScript.js
@@ -1,17 +1,17 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
-    var x = document.getElementById("myTopnav");
+// function myFunction() {
+//     var x = document.getElementById("myTopnav");
 
-    if (x.className === "topnav") {
-        x.className += " responsive";
-        x.style.display = "none";
-        // document.getElementById('largeMenu').innerHTML = "";
-    } else {
-        x.className = "topnav";
-        x.style.display = "block";
-    }
+//     if (x.className === "topnav") {
+//         x.className += " responsive";
+//         x.style.display = "none";
+//         // document.getElementById('largeMenu').innerHTML = "";
+//     } else {
+//         x.className = "topnav";
+//         x.style.display = "block";
+//     }
 
-}
+// }
 
 function closeSidebar() {
     let y = document.getElementById("myTopnav");
@@ -41,30 +41,30 @@ function goContact() {
 
 window.onscroll = function () { runOnscroll() };
 
-function runOnscroll() {
-    if (window.innerWidth > 600) {
-        if (document.documentElement.scrollTop > 900) {
-            navInner.classList.add("w3-black");
-            // navInner.style.opacity = "0.5";
-            brandLogo.style.opacity = "1";
+// function runOnscroll() {
+//     if (window.innerWidth > 600) {
+//         if (document.documentElement.scrollTop > 900) {
+//             navInner.classList.add("w3-black");
+//             // navInner.style.opacity = "0.5";
+//             // brandLogo.style.opacity = "1";
 
-        } else {
-            navContainer.classList.add("nav-back");
-            navInner.classList.remove("w3-black");
-            brandLogo.style.opacity = "1";
-        }
-    } else {
-        if (document.documentElement.scrollTop > 245) {
-            navInner.classList.add("w3-black");
-            // navInner.style.opacity = "0.5";
-            brandLogo.style.opacity = "1";
-        } else {
-            navContainer.classList.add("nav-back");
-            navInner.classList.remove("w3-black");
-            brandLogo.style.opacity = "1";
-        }
-    }
-}
+//         } else {
+//             navContainer.classList.add("nav-back");
+//             navInner.classList.remove("w3-black");
+//             // brandLogo.style.opacity = "1";
+//         }
+//     } else {
+//         if (document.documentElement.scrollTop > 245) {
+//             navInner.classList.add("w3-black");
+//             // navInner.style.opacity = "0.5";
+//             // brandLogo.style.opacity = "1";
+//         } else {
+//             navContainer.classList.add("nav-back");
+//             navInner.classList.remove("w3-black");
+//             // brandLogo.style.opacity = "1";
+//         }
+//     }
+// }
 
 
 window.onload = function () {
@@ -84,3 +84,36 @@ window.onload = function () {
     //     d.getElementsByTagName('head')[0].appendChild(script);
     // }(document));
 };
+// Automatic Slideshow - change image every 4 seconds
+var myIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    myIndex++;
+    if (myIndex > x.length) { myIndex = 1 }
+    x[myIndex - 1].style.display = "block";
+    setTimeout(carousel, 4000);
+}
+
+// Used to toggle the menu on small screens when clicking on the menu button
+function myFunction() {
+    var x = document.getElementById("navDemo");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
+
+// When the user clicks anywhere outside of the modal, close it
+var modal = document.getElementById('ticketModal');
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
